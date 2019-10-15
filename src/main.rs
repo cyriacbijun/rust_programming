1bdd041e2a3e63294a2854f64fd7572fa54cde6c
Cyriac <cyriacbijun@gmail.com>
2019-10-15 21:55:47 +0530

    Added loop to program to keep on guessing

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,24 @@ use rand::Rng;
 use std::cmp::Ordering;
 
 fn main() {
-    let mut guess = String::new();
+    
     let secret_number = rand::thread_rng().gen_range(1, 101);
-    println!("Hello, enter your guess");
-    io::stdin().read_line(&mut guess).expect("Line not read");
-    let guess: u32 = guess.trim().parse()
-        .expect("Please type a number!");
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+    loop{
+        println!("Please enter your guess");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("Line not read");
+        let guess: u32 = match guess.trim().parse(){
+                Ok(num) => num,
+                Err(_) => break,
+            };
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            } 
+        }
     }
-    println!("Here is your guess: {} and the actual number : {} ",guess,secret_number);
 
 }
